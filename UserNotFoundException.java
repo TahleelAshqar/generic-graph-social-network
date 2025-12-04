0dd9ed69e3d7a3213048f76db87a3f99e2703490
Tahleel Ashqar <tahleel.as1@gmail.com>
2025-12-04 10:31:11 +0200

    Create UserNotFoundException.java
    Exception thrown when an operation in the social network references a user who does not exist. 
    Acts as a user-level wrapper for missing vertices in the underlying graph.

---
+++ UserNotFoundException.java
@@ -0,0 +1,10 @@
+public class UserNotFoundException extends Exception {
+
+    public UserNotFoundException() {
+        super();
+    }
+
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
